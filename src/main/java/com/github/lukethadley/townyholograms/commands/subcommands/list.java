8bfabaf280005460f4d898aeeaf47eb6e6427495
lukehadley <lukehadley2001@gmail.com>
2021-01-11 11:33:33 +0000

    Added aditional information to info command
    Modified 'info' command such that it will display how many holograms are in the town out of how many are allowed in the town.

--- src/main/java/com/github/lukethadley/townyholograms/commands/subcommands/list.java
+++ src/main/java/com/github/lukethadley/townyholograms/commands/subcommands/list.java
@@ -4,8 +4,11 @@ import com.github.lukethadley.townyholograms.Strings;
 import com.github.lukethadley.townyholograms.TownyHolograms;
 import com.github.lukethadley.townyholograms.commands.SubCommand;
 import com.github.lukethadley.townyholograms.storage.ConfigValues;
+import com.github.lukethadley.townyholograms.storage.HologramAllowance;
 import com.github.lukethadley.townyholograms.storage.HologramItem;
 import com.github.lukethadley.townyholograms.storage.database.DatabaseConnection;
+import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandException;
 import org.bukkit.command.CommandSender;
@@ -33,14 +36,34 @@ public class list extends SubCommand {
 
     @Override
     public void execute(TownyHolograms plugin, CommandSender sender, String label, String[] args, ConfigValues configValues, DatabaseConnection databaseConnection) throws CommandException {
-        Player player = (Player) sender;
-        ArrayList<HologramItem> holograms = databaseConnection.getHolograms(plugin.getTownFromPlayer(player).getUuid().toString());
-        sender.sendMessage(ChatColor.GOLD + ".oOo.___________.[ " + ChatColor.YELLOW + plugin.getTownFromPlayer(player).getName() + " Holograms " + ChatColor.GOLD + "].___________.oOo.");
-        if (holograms.size() == 0){
-            sender.sendMessage(ChatColor.DARK_GREEN + "Your town has no holograms at the moment.");
-        }
-        for (HologramItem hologram : holograms){
-            sender.sendMessage(" " + ChatColor.DARK_AQUA + hologram.getName() + ChatColor.GRAY + " - " + ChatColor.AQUA + hologram.getLocation().getWorld().getName() + " X:" + String.format("%.03f", hologram.getLocation().getX()) + " Y:" + String.format("%.03f", hologram.getLocation().getY()) + " Z:" + String.format("%.03f", hologram.getLocation().getZ()));
+        try {
+
+            Player player = (Player) sender;
+
+            ArrayList<HologramItem> holograms = databaseConnection.getHolograms(plugin.getTownFromPlayer(player).getUuid().toString());
+            sender.sendMessage(ChatColor.GOLD + ".oOo.___________.[ " + ChatColor.YELLOW + plugin.getTownFromPlayer(player).getName() + " Holograms " + ChatColor.GOLD + "].___________.oOo.");
+            if (holograms.size() == 0) {
+                sender.sendMessage(ChatColor.DARK_GREEN + "Your town has no holograms at the moment.");
+                return;
+            }
+
+            int townResidentCount = TownyAPI.getInstance().getTownBlock(player.getLocation()).getTown().getNumResidents();
+            
+            HologramAllowance closest = new HologramAllowance(0, 0);
+            for (HologramAllowance allowance : plugin.prices) {
+
+                if (allowance.getNumberOfResidents() <= townResidentCount && allowance.getNumberOfResidents() > closest.getNumberOfResidents()) {
+                    closest = allowance;
+                }
+            }
+
+            sender.sendMessage(ChatColor.DARK_GREEN + "Your town is using " + ChatColor.GREEN + holograms.size() + ChatColor.DARK_GREEN + "/" + ChatColor.GREEN + closest.getNumberOfHolograms() + ChatColor.DARK_GREEN + " allowed holograms.");
+            for (HologramItem hologram : holograms) {
+                sender.sendMessage(" " + ChatColor.DARK_AQUA + hologram.getName() + ChatColor.GRAY + " - " + ChatColor.AQUA + hologram.getLocation().getWorld().getName() + " X:" + String.format("%.03f", hologram.getLocation().getX()) + " Y:" + String.format("%.03f", hologram.getLocation().getY()) + " Z:" + String.format("%.03f", hologram.getLocation().getZ()));
+            }
+
+        } catch (NotRegisteredException e){
+            plugin.getLogger().warning("An issue occurred while listing town holograms.");
         }
     }
 }
